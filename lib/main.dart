3f2d7ba5243f88d5a93726667f3dd2b6eae07c2c
Sanchez <bryanschez@gmail.com>
2026-01-18 17:48:42 -0500

    style: changed the color of the widgets of the main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/home_page.dart';
 
 void main() {
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
+        textTheme: GoogleFonts.dosisTextTheme(),
       ),
       home: const HomePage(),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -25,138 +25,144 @@ class _HomePageState extends ConsumerState<HomePage> {
       backgroundColor: const Color(0x9092AAFF),
       body: SafeArea(
         child: Container(
-            color: const Color.fromARGB(0, 41, 55, 182),
             padding: const EdgeInsets.only(top: 10, left: 25),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(
-                    width: _leftPanelWidth,
-                    key: const Key("leftPanel"),
-                    child: Container(
-                      padding: const EdgeInsets.all(6),
-                      color: const Color(0XDEDEDEFF).withValues(alpha: 0.6),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                              key: const Key('header'),
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text(
-                                  'Experiences',
-                                  style: GoogleFonts.dosis(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w700,
-                                    color: Colors.black.withValues(alpha: 0.3),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                      width: _leftPanelWidth,
+                      key: const Key("leftPanel"),
+                      child: Container(
+                        padding: const EdgeInsets.all(6),
+                        color: const Color.fromRGBO(222, 222, 222, 0.8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                                key: const Key('header'),
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    'Experiences',
+                                    style: GoogleFonts.dosis(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w700,
+                                      color:
+                                          Colors.black.withValues(alpha: 0.3),
+                                    ),
                                   ),
-                                ),
-                                SizedBox(
-                                  width: 50,
-                                  child: IconButton(
-                                    icon: const Icon(Icons.add),
-                                    onPressed: () {
-                                      setState(() {
-                                        _showForm = !_showForm;
-                                      });
-                                    },
+                                  SizedBox(
+                                    width: 50,
+                                    child: IconButton(
+                                      icon: const Icon(Icons.add),
+                                      onPressed: () {
+                                        setState(() {
+                                          _showForm = !_showForm;
+                                        });
+                                      },
+                                    ),
                                   ),
-                                ),
-                              ]),
-                          if (_showForm) ...[
-                            const SizedBox(height: 16),
-                            AddExperienceForm(
-                              onSubmit: () {
-                                setState(() {
-                                  _showForm = false;
-                                });
-                              },
+                                ]),
+                            if (_showForm) ...[
+                              const SizedBox(height: 16),
+                              AddExperienceForm(
+                                onSubmit: () {
+                                  setState(() {
+                                    _showForm = false;
+                                  });
+                                },
+                              ),
+                            ],
+                            const SizedBox(height: 15),
+                            Expanded(
+                              key: const Key('experienceList'),
+                              child: experiences.isEmpty
+                                  ? const Center(
+                                      child: Text(
+                                        'Aucune expérience',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          color:
+                                              Color.fromARGB(255, 156, 63, 63),
+                                        ),
+                                      ),
+                                    )
+                                  : ListView.builder(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 16),
+                                      itemCount: experiences.length,
+                                      itemBuilder: (context, index) {
+                                        final experience = experiences[index];
+                                        return Padding(
+                                          padding:
+                                              const EdgeInsets.only(bottom: 16),
+                                          child: ExperienceCard(
+                                            index: index,
+                                            experience: experience,
+                                            onView: () {
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      ExperienceDetailPage(
+                                                          experienceIndex:
+                                                              index),
+                                                ),
+                                              );
+                                            },
+                                            onDelete: () {
+                                              _showDeleteConfirmation(
+                                                  context, index);
+                                            },
+                                            onEditTitle: () {
+                                              _showEditTitleDialog(
+                                                  context, index);
+                                            },
+                                          ),
+                                        );
+                                      },
+                                    ),
                             ),
                           ],
-                          const SizedBox(height: 15),
-                          Expanded(
-                            key: const Key('experienceList'),
-                            child: experiences.isEmpty
-                                ? const Center(
-                                    child: Text(
-                                      'Aucune expérience',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        color: Color.fromARGB(255, 156, 63, 63),
-                                      ),
-                                    ),
-                                  )
-                                : ListView.builder(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 16),
-                                    itemCount: experiences.length,
-                                    itemBuilder: (context, index) {
-                                      final experience = experiences[index];
-                                      return Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 16),
-                                        child: ExperienceCard(
-                                          index: index,
-                                          experience: experience,
-                                          onView: () {
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                builder: (context) =>
-                                                    ExperienceDetailPage(
-                                                        experienceIndex: index),
-                                              ),
-                                            );
-                                          },
-                                          onDelete: () {
-                                            _showDeleteConfirmation(
-                                                context, index);
-                                          },
-                                          onEditTitle: () {
-                                            _showEditTitleDialog(
-                                                context, index);
-                                          },
-                                        ),
-                                      );
-                                    },
-                                  ),
-                          ),
-                        ],
-                      ),
-                    )),
-                MouseRegion(
-                  cursor: SystemMouseCursors.resizeColumn,
-                  child: GestureDetector(
-                    onHorizontalDragUpdate: (details) {
-                      setState(() {
-                        _leftPanelWidth += details.delta.dx;
-                        if (_leftPanelWidth < 200) _leftPanelWidth = 200;
-                      });
-                    },
-                    child: Container(
-                      width: 10,
-                      color: const Color(0XDEDEDEFF),
-                      alignment: Alignment.center,
+                        ),
+                      )),
+                  MouseRegion(
+                    cursor: SystemMouseCursors.resizeColumn,
+                    child: GestureDetector(
+                      onHorizontalDragUpdate: (details) {
+                        setState(() {
+                          _leftPanelWidth += details.delta.dx;
+                          if (_leftPanelWidth < 200) _leftPanelWidth = 200;
+                        });
+                      },
                       child: Container(
-                        width: 0,
-                        color: const Color(0XDEDEDEFF),
+                        width: 10,
+                        color: const Color.fromRGBO(230, 230, 230, 0.8),
+                        alignment: Alignment.center,
+                        child: Container(
+                          width: 0,
+                          color: const Color.fromRGBO(230, 230, 230, 0.8),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    key: const Key('rightPanel'),
-                    color: const Color(0XDEDEDEFF),
-                    child: const Column(children: [
-                      Text(
-                        'Right panel',
-                      ),
-                    ]),
-                  ),
-                )
-              ],
+                  Expanded(
+                    child: Container(
+                      key: const Key('rightPanel'),
+                      color: const Color.fromRGBO(230, 230, 230, 0.8),
+                      child: const Column(children: [
+                        Text(
+                          'Right panel',
+                        ),
+                      ]),
+                    ),
+                  )
+                ],
+              ),
             )),
       ),
     );
